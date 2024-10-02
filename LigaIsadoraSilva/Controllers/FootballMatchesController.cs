3fe91e1485ba0965184a5f74aed24a0be2bf4ac9
Isadora Silva <marchiisadora@gmail.com>
2024-10-02 12:40:32 +0100

    Controllers, views and style

---
+++ LigaIsadoraSilva/Controllers/FootballMatchesController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LigaIsadoraSilva.Data;
+using LigaIsadoraSilva.Data.Entities;
+
+namespace LigaIsadoraSilva.Controllers
+{
+    public class FootballMatchesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public FootballMatchesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: FootballMatches
+        public async Task<IActionResult> Index()
+        {
+            var dataContext = _context.Games.Include(f => f.HomeTeam).Include(f => f.VisitTeam);
+            return View(await dataContext.ToListAsync());
+        }
+
+        // GET: FootballMatches/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var footballMatch = await _context.Games
+                .Include(f => f.HomeTeam)
+                .Include(f => f.VisitTeam)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (footballMatch == null)
+            {
+                return NotFound();
+            }
+
+            return View(footballMatch);
+        }
+
+        // GET: FootballMatches/Create
+        public IActionResult Create()
+        {
+            ViewData["HomeTeamId"] = new SelectList(_context.Clubs, "Id", "Coach");
+            ViewData["VisitTeamId"] = new SelectList(_context.Clubs, "Id", "Coach");
+            return View();
+        }
+
+        // POST: FootballMatches/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(FootballMatch footballMatch)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(footballMatch);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HomeTeamId"] = new SelectList(_context.Clubs, "Id", "Coach", footballMatch.HomeTeamId);
+            ViewData["VisitTeamId"] = new SelectList(_context.Clubs, "Id", "Coach", footballMatch.VisitTeamId);
+            return View(footballMatch);
+        }
+
+        // GET: FootballMatches/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var footballMatch = await _context.Games.FindAsync(id);
+            if (footballMatch == null)
+            {
+                return NotFound();
+            }
+            ViewData["HomeTeamId"] = new SelectList(_context.Clubs, "Id", "Coach", footballMatch.HomeTeamId);
+            ViewData["VisitTeamId"] = new SelectList(_context.Clubs, "Id", "Coach", footballMatch.VisitTeamId);
+            return View(footballMatch);
+        }
+
+        // POST: FootballMatches/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, FootballMatch footballMatch)
+        {
+            if (id != footballMatch.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(footballMatch);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FootballMatchExists(footballMatch.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HomeTeamId"] = new SelectList(_context.Clubs, "Id", "Coach", footballMatch.HomeTeamId);
+            ViewData["VisitTeamId"] = new SelectList(_context.Clubs, "Id", "Coach", footballMatch.VisitTeamId);
+            return View(footballMatch);
+        }
+
+        // GET: FootballMatches/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var footballMatch = await _context.Games
+                .Include(f => f.HomeTeam)
+                .Include(f => f.VisitTeam)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (footballMatch == null)
+            {
+                return NotFound();
+            }
+
+            return View(footballMatch);
+        }
+
+        // POST: FootballMatches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var footballMatch = await _context.Games.FindAsync(id);
+            if (footballMatch != null)
+            {
+                _context.Games.Remove(footballMatch);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FootballMatchExists(int id)
+        {
+            return _context.Games.Any(e => e.Id == id);
+        }
+    }
+}
--- LigaIsadoraSilva/Controllers/PlayersController.cs
+++ LigaIsadoraSilva/Controllers/PlayersController.cs
@@ -1,26 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using LigaIsadoraSilva.Data;
 using LigaIsadoraSilva.Data.Entities;
 
 namespace LigaIsadoraSilva.Controllers
 {
-    public class FootballTeamsController : Controller
+    public class PlayersController : Controller
     {
         private readonly DataContext _context;
 
-        public FootballTeamsController(DataContext context)
+        public PlayersController(DataContext context)
         {
             _context = context;
         }
 
-        // GET: FootballTeams
+        // GET: Players
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Clubs.ToListAsync());
+            return View(await _context.Players.ToListAsync());
         }
 
-        // GET: FootballTeams/Details/5
+        // GET: Players/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -28,39 +33,39 @@ namespace LigaIsadoraSilva.Controllers
                 return NotFound();
             }
 
-            var footballTeam = await _context.Clubs
+            var player = await _context.Players
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (footballTeam == null)
+            if (player == null)
             {
                 return NotFound();
             }
 
-            return View(footballTeam);
+            return View(player);
         }
 
-        // GET: FootballTeams/Create
+        // GET: Players/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: FootballTeams/Create
+        // POST: Players/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(FootballTeam footballTeam)
+        public async Task<IActionResult> Create(Player player)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(footballTeam);
+                _context.Add(player);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(footballTeam);
+            return View(player);
         }
 
-        // GET: FootballTeams/Edit/5
+        // GET: Players/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -68,22 +73,22 @@ namespace LigaIsadoraSilva.Controllers
                 return NotFound();
             }
 
-            var footballTeam = await _context.Clubs.FindAsync(id);
-            if (footballTeam == null)
+            var player = await _context.Players.FindAsync(id);
+            if (player == null)
             {
                 return NotFound();
             }
-            return View(footballTeam);
+            return View(player);
         }
 
-        // POST: FootballTeams/Edit/5
+        // POST: Players/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, FootballTeam footballTeam)
+        public async Task<IActionResult> Edit(int id, Player player)
         {
-            if (id != footballTeam.Id)
+            if (id != player.Id)
             {
                 return NotFound();
             }
@@ -92,12 +97,12 @@ namespace LigaIsadoraSilva.Controllers
             {
                 try
                 {
-                    _context.Update(footballTeam);
+                    _context.Update(player);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!FootballTeamExists(footballTeam.Id))
+                    if (!PlayerExists(player.Id))
                     {
                         return NotFound();
                     }
@@ -108,10 +113,10 @@ namespace LigaIsadoraSilva.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(footballTeam);
+            return View(player);
         }
 
-        // GET: FootballTeams/Delete/5
+        // GET: Players/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -119,34 +124,34 @@ namespace LigaIsadoraSilva.Controllers
                 return NotFound();
             }
 
-            var footballTeam = await _context.Clubs
+            var player = await _context.Players
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (footballTeam == null)
+            if (player == null)
             {
                 return NotFound();
             }
 
-            return View(footballTeam);
+            return View(player);
         }
 
-        // POST: FootballTeams/Delete/5
+        // POST: Players/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var footballTeam = await _context.Clubs.FindAsync(id);
-            if (footballTeam != null)
+            var player = await _context.Players.FindAsync(id);
+            if (player != null)
             {
-                _context.Clubs.Remove(footballTeam);
+                _context.Players.Remove(player);
             }
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool FootballTeamExists(int id)
+        private bool PlayerExists(int id)
         {
-            return _context.Clubs.Any(e => e.Id == id);
+            return _context.Players.Any(e => e.Id == id);
         }
     }
 }
